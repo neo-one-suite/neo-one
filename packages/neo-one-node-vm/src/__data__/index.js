e4db9b67eb033a4e21d8eb04b828dedad494e5d6
Alex <alexdicarlo@gmail.com>
2018-02-16 15:49:24 -0800

    More tests and fixes for vm

--- packages/neo-one-node-vm/src/__data__/index.js
+++ packages/neo-one-node-vm/src/__data__/index.js
@@ -1,5 +1,5 @@
 /* @flow */
-// eslint-disable-next-line
+export { default as createBlockchain } from './createBlockchain';
 export { default as keys } from './keys';
 export * as transactions from './transactions';
 export * as testUtils from './utils';
