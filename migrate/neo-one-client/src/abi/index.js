ca8b4ddf43caeebbd0a7d5265b275729481639ae
Alex <alexdicarlo@gmail.com>
2018-02-26 02:23:02 -0800

    Remove old neo-one-client code

--- migrate/neo-one-client/src/abi/index.js
+++
@@ -1,6 +0,0 @@
-/* @flow */
-import NEP5 from './NEP5';
-
-export default {
-  NEP5,
-};
--- migrate/neo-one-client/src/json/index.js
+++
@@ -1,8 +0,0 @@
-/* @flow */
-export {
-  default as JSONRPCBasicClientProvider,
-} from './JSONRPCBasicClientProvider';
-export { default as JSONRPCClientProvider } from './JSONRPCClientProvider';
-export { default as JSONRPCHTTPProvider } from './JSONRPCHTTPProvider';
-
-export type { JSONRPCProvider } from './JSONRPCProvider';
