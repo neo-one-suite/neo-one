665c3d575eb782ae7e296e43fda3f8fa779d5abc
Alex <alexdicarlo@gmail.com>
2018-03-18 00:28:16 -0700

    Add create net main/test e2e

--- packages/neo-one-server-plugin-network/src/__e2e__/createNetwork.test.js
+++ packages/neo-one-server-plugin-network/src/__e2e__/createNetwork.test.js
@@ -10,10 +10,47 @@ describe('create network', () => {
     const description = one.parseJSON(output);
     expect(description[0]).toEqual(['Name', 'priv']);
     expect(description[1]).toEqual(['Type', 'private']);
+    // Name
     expect(description[3][1].table[1][0]).toEqual('priv-0');
+    // Live
     expect(description[3][1].table[1][1]).toEqual('Yes');
+    // Ready
     expect(description[3][1].table[1][2]).toEqual('Yes');
+    // Height
     expect(parseInt(description[3][1].table[1][5], 10)).toBeGreaterThan(1);
+    // Peers
     expect(description[3][1].table[1][6]).toEqual('0');
+  });
+
+  test('create network main', async () => {
+    await one.execute('create network main');
+
+    await new Promise(resolve => setTimeout(() => resolve(), 10000));
+    const output = await one.execute('describe network main --json');
+
+    const description = one.parseJSON(output);
+    expect(description[0]).toEqual(['Name', 'main']);
+    expect(description[1]).toEqual(['Type', 'main']);
+    expect(description[3][1].table[1][0]).toEqual('main');
+    expect(description[3][1].table[1][1]).toEqual('Yes');
+    expect(description[3][1].table[1][2]).toEqual('No');
+    expect(parseInt(description[3][1].table[1][5], 10)).toBeGreaterThan(1);
+    expect(parseInt(description[3][1].table[1][6], 10)).toBeGreaterThan(1);
+  });
+
+  test('create network test', async () => {
+    await one.execute('create network test');
+
+    await new Promise(resolve => setTimeout(() => resolve(), 10000));
+    const output = await one.execute('describe network test --json');
+
+    const description = one.parseJSON(output);
+    expect(description[0]).toEqual(['Name', 'test']);
+    expect(description[1]).toEqual(['Type', 'test']);
+    expect(description[3][1].table[1][0]).toEqual('test');
+    expect(description[3][1].table[1][1]).toEqual('Yes');
+    expect(description[3][1].table[1][2]).toEqual('No');
+    expect(parseInt(description[3][1].table[1][5], 10)).toBeGreaterThan(1);
+    expect(parseInt(description[3][1].table[1][6], 10)).toBeGreaterThan(1);
   });
 });
