b3a7ae939c0ff810e9a09dab6ab376405bd91b94
Dave M <david.mathiesen@neotracker.io>
2018-06-15 12:10:10 -0700

    Not ready for merge, setting down in favor of a code-walk later

--- packages/neo-one-smart-contract-compiler/src/compile/expression/ObjectLiteralExpressionCompiler.ts
+++ packages/neo-one-smart-contract-compiler/src/compile/expression/ObjectLiteralExpressionCompiler.ts
@@ -18,6 +18,7 @@ export default class ObjectLiteralExpressionCompiler extends NodeCompiler<
     const options = sb.pushValueOptions(optionsIn);
     // [objectVal]
     sb.emitHelper(node, options, sb.helpers.createObject);
+
     node.getProperties().forEach((prop) => {
       // [objectVal, objectVal]
       sb.emitOp(node, 'DUP');
@@ -45,12 +46,85 @@ export default class ObjectLiteralExpressionCompiler extends NodeCompiler<
               property: InternalFunctionProperties.CALL,
             }),
           );
-        } else {
+        } else  {
           sb.reportUnsupported(prop);
         }
         // [objectVal]
         sb.emitHelper(prop, options, sb.helpers.setDataPropertyObjectProperty);
+      } else if (TypeGuards.isSetAccessorDeclaration(prop)){
+        // [objectVal, objectVal, 'prototype', fobjectVal, fobjectVal]
+        sb.emitOp(prop, 'DUP');
+
+        // [name, objectVal, objectVal, 'prototype', fobjectVal, fobjectVal]
+        sb.emitPushString(prop, prop.getName());
+
+        // [farr, name, objectVal, objectVal, 'prototype', fobjectVal, fobjectVal]
+        sb.emitHelper(prop, options, sb.helpers.createCallArray);
+
+        // [methodObjectVal, name, objectVal, objectVal, 'prototype', fobjectVal, fobjectVal]
+        sb.emitHelper(
+          node,
+          options,
+          sb.helpers.createFunctionObject({
+            property: InternalFunctionProperties.CALL,
+          }),
+        );
+
+        const getAccessor = prop.getGetAccessor(); // <<<< HERE
+
+        const hasGet = getAccessor != null; // <<<<<<<< ????
+        if (getAccessor != null) {
+          sb.emitHelper(getAccessor, options, sb.helpers.createCallArray);
+          sb.emitHelper(
+            node,
+            options,
+            sb.helpers.createFunctionObject({
+              property: InternalFunctionProperties.CALL,
+            }),
+          );
+        }
+
+        // [objectVal, 'prototype', fobjectVal, fobjectVal]
+        sb.emitHelper(
+          prop,
+          options,
+          sb.helpers.setAccessorPropertyObjectProperty({
+            hasSet: true,
+            hasGet,
+          }),
+        );
+
+      } else if (TypeGuards.isGetAccessorDeclaration(prop)) {
+
+        // [objectVal, objectVal, 'prototype', fobjectVal, fobjectVal]
+        sb.emitOp(prop, 'DUP');
+
+        // [name, objectVal, objectVal, 'prototype', fobjectVal, fobjectVal]
+        sb.emitPushString(prop, prop.getName());
+
+        // [farr, name, objectVal, objectVal, 'prototype', fobjectVal, fobjectVal]
+        sb.emitHelper(prop, options, sb.helpers.createCallArray);
+
+        // [methodObjectVal, name, objectVal, objectVal, 'prototype', fobjectVal, fobjectVal]
+        sb.emitHelper(
+          node, // Changed DECL (class declaration?) to NODE which represents this current object?
+          options,
+          sb.helpers.createFunctionObject({
+            property: InternalFunctionProperties.CALL,
+          }),
+        );
+
+        // [objectVal, 'prototype', fobjectVal, fobjectVal]
+        sb.emitHelper(
+          prop,
+          options,
+          sb.helpers.setAccessorPropertyObjectProperty({
+            hasSet: false,
+            hasGet: true,
+          }),
+        );
       } else {
+
         sb.reportUnsupported(prop);
       }
     });
