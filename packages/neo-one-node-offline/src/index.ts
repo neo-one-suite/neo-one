d0114381b3b90905a0d65f8e276a953ba5f2b51c
Alex <alexdicarlo@gmail.com>
2018-06-15 19:59:18 -0700

    Convert @neo-one/node-offline to typescript

--- packages/neo-one-node-offline/src/index.ts
+++ packages/neo-one-node-offline/src/index.ts
@@ -1,20 +1,15 @@
-/* @flow */
-import {
-  type DeserializeWireContext,
-  BinaryReader,
-  Block,
-} from '@neo-one/client-core';
-import type { Blockchain } from '@neo-one/node-core';
+import { BinaryReader, Block, DeserializeWireContext } from '@neo-one/client-core';
+import { Blockchain } from '@neo-one/node-core';
 import { CustomError } from '@neo-one/utils';
-import { type Readable, type Writable, Transform } from 'stream';
-
-import _ from 'lodash';
 import fs from 'fs';
+import _ from 'lodash';
+import { Readable, Transform, Writable } from 'stream';
 import zlib from 'zlib';
 
 export class InvalidBlockTransformEncodingError extends CustomError {
-  code: string;
-  constructor() {
+  public readonly code: string;
+
+  public constructor() {
     super('Invalid Block Transform Encoding.');
     this.code = 'INVALID_BLOCK_TRANSFORM_ENCODING';
   }
@@ -23,52 +18,52 @@ export class InvalidBlockTransformEncodingError extends CustomError {
 const SIZE_OF_INT32 = 4;
 
 class BlockTransform extends Transform {
-  context: DeserializeWireContext;
-  buffer: Buffer;
+  public readonly context: DeserializeWireContext;
+  public mutableBuffer: Buffer;
 
-  constructor(context: DeserializeWireContext) {
+  public constructor(context: DeserializeWireContext) {
     super({ readableObjectMode: true });
     this.context = context;
-    this.buffer = Buffer.from([]);
+    this.mutableBuffer = Buffer.from([]);
   }
 
-  _transform(
+  public _transform(
     chunk: Buffer | string,
     encoding: string,
-    callback: (error: ?Error, data?: Buffer | string) => void,
+    callback: ((error: Error | undefined, data?: Buffer | string) => void),
   ): void {
     if (typeof chunk === 'string' || encoding !== 'buffer') {
       throw new InvalidBlockTransformEncodingError();
     }
 
-    this.buffer = Buffer.concat([this.buffer, chunk]);
+    this.mutableBuffer = Buffer.concat([this.mutableBuffer, chunk]);
     try {
-      const { remainingBuffer, blocks } = this._processBuffer(
-        new BinaryReader(this.buffer),
-      );
-      this.buffer = remainingBuffer;
-      blocks.reverse().forEach((block) => this.push((block: $FlowFixMe)));
-      callback(null);
+      const { remainingBuffer, mutableBlocks } = this.processBuffer(new BinaryReader(this.mutableBuffer));
+
+      this.mutableBuffer = remainingBuffer;
+      mutableBlocks.reverse();
+      mutableBlocks.forEach((block) => this.push(block));
+      callback(undefined);
     } catch (error) {
       callback(error);
     }
   }
 
-  _processBuffer(
+  private processBuffer(
     reader: BinaryReader,
-  ): {|
-    remainingBuffer: Buffer,
-    blocks: Array<Block>,
-  |} {
+  ): {
+    readonly remainingBuffer: Buffer;
+    readonly mutableBlocks: Block[];
+  } {
     if (reader.remaining < SIZE_OF_INT32) {
-      return { remainingBuffer: reader.remainingBuffer, blocks: [] };
+      return { remainingBuffer: reader.remainingBuffer, mutableBlocks: [] };
     }
 
     const length = reader.clone().readInt32LE();
 
     // Not sure why this doesn't work properly with just length...
     if (reader.remaining + SIZE_OF_INT32 < length * 2) {
-      return { remainingBuffer: reader.remainingBuffer, blocks: [] };
+      return { remainingBuffer: reader.remainingBuffer, mutableBlocks: [] };
     }
 
     reader.readInt32LE();
@@ -76,16 +71,17 @@ class BlockTransform extends Transform {
       context: this.context,
       reader,
     });
-    const { remainingBuffer, blocks } = this._processBuffer(reader);
-    blocks.push(block);
-    return { remainingBuffer, blocks };
+
+    const { remainingBuffer, mutableBlocks } = this.processBuffer(reader);
+    mutableBlocks.push(block);
+
+    return { remainingBuffer, mutableBlocks };
   }
 }
-
-export type Chain = {|
-  format: 'raw' | 'zip',
-  path: string,
-|};
+export interface Chain {
+  readonly format: 'raw' | 'zip';
+  readonly path: string;
+}
 
 const getStream = (chain: Chain): Readable => {
   const stream = fs.createReadStream(chain.path);
@@ -98,9 +94,9 @@ const getStream = (chain: Chain): Readable => {
 
 const getCount = async (stream: Readable): Promise<number> => {
   let count = stream.read(4);
-  while (count == null) {
-    // eslint-disable-next-line
-    await new Promise((resolve) => setTimeout(() => resolve(), 250));
+  // tslint:disable-next-line no-loop-statement
+  while (count === undefined) {
+    await new Promise<void>((resolve) => setTimeout(resolve, 250));
     count = stream.read(4);
   }
 
@@ -111,11 +107,11 @@ const getCount = async (stream: Readable): Promise<number> => {
 export const loadChain = async ({
   blockchain,
   chain,
-}: {|
-  blockchain: Blockchain,
-  chain: Chain,
-|}): Promise<void> =>
-  new Promise((resolve, reject) => {
+}: {
+  readonly blockchain: Blockchain;
+  readonly chain: Chain;
+}): Promise<void> =>
+  new Promise<void>((resolve, reject) => {
     const stream = getStream(chain);
     const transform = new BlockTransform(blockchain.deserializeWireContext);
 
@@ -154,7 +150,7 @@ export const loadChain = async ({
     let start = Date.now();
     let paused = false;
     const trackIndex = blockchain.currentBlockIndex;
-    transform.on('data', (block) => {
+    transform.on('data', (block: Block) => {
       pending += 1;
       blockchain
         .persistBlock({ block, unsafe: true })
@@ -166,20 +162,16 @@ export const loadChain = async ({
           }
 
           if (block.index === trackIndex) {
-            // eslint-disable-next-line
-            console.log(
-              `Loaded chain to current index in ${Date.now() - start} ms`,
-            );
+            // tslint:disable-next-line no-console
+            console.log(`Loaded chain to current index in ${Date.now() - start} ms`);
+
             start = Date.now();
           } else if (block.index > trackIndex) {
             processed += 1;
             if (processed >= 100) {
-              // eslint-disable-next-line
-              console.log(
-                `Processed ${processed} blocks in ${Date.now() - start} ms (${
-                  block.index
-                })`,
-              );
+              // tslint:disable-next-line no-console
+              console.log(`Processed ${processed} blocks in ${Date.now() - start} ms (${block.index})`);
+
               processed = 0;
               start = Date.now();
             }
@@ -206,32 +198,25 @@ export const loadChain = async ({
       .catch(reject);
   });
 
-const writeOut = async (
-  blockchain: Blockchain,
-  out: Writable,
-  height: number,
-): Promise<void> => {
+const writeOut = async (blockchain: Blockchain, out: Writable, height: number): Promise<void> => {
   let processed = 0;
   let start = Date.now();
+  // tslint:disable-next-line no-loop-statement
   for (const chunk of _.chunk(_.range(0, height), 10000)) {
     // eslint-disable-next-line
-    const blocks = await Promise.all(
-      chunk.map((index) => blockchain.block.get({ hashOrIndex: index })),
-    );
+    const blocks = await Promise.all(chunk.map(async (index) => blockchain.block.get({ hashOrIndex: index })));
+
+    // tslint:disable-next-line no-loop-statement
     for (const block of blocks) {
       const buffer = block.serializeWire();
       const length = Buffer.alloc(4, 0);
-      length.writeInt32LE(buffer.length, 0);
-      // eslint-disable-next-line
-      await new Promise((resolve) => out.write(length, () => resolve()));
-      // eslint-disable-next-line
-      await new Promise((resolve) => out.write(buffer, () => resolve()));
+      await new Promise<void>((resolve) => out.write(length, resolve));
+      await new Promise<void>((resolve) => out.write(buffer, resolve));
       processed += 1;
       if (processed >= 100000) {
-        // eslint-disable-next-line
-        console.log(
-          `Processed ${processed} blocks in ${Date.now() - start} ms`,
-        );
+        // tslint:disable-next-line no-console
+        console.log(`Processed ${processed} blocks in ${Date.now() - start} ms`);
+
         processed = 0;
         start = Date.now();
       }
@@ -244,11 +229,11 @@ export const dumpChain = async ({
   blockchain,
   path,
 }: {
-  blockchain: Blockchain,
-  path: string,
+  readonly blockchain: Blockchain;
+  readonly path: string;
 }): Promise<void> => {
   const height = blockchain.currentBlockIndex;
-  await new Promise((resolve, reject) => {
+  await new Promise<void>((resolve, reject) => {
     const out = fs.createWriteStream(path);
     out.once('open', () => {
       const count = Buffer.alloc(4, 0);
