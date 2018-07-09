21b9526f6e2640a5326fb1f5466886ae618bff1e
davemneo <39564353+davemneo@users.noreply.github.com>
2018-07-09 07:23:37 -0700

    Consolidate shared logic between setupTest and startNode (#284)

--- packages/neo-one-smart-contract-compiler/src/test/index.ts
+++ packages/neo-one-smart-contract-compiler/src/test/index.ts
@@ -2,3 +2,4 @@ export * from './cleanupTest';
 export * from './setupBasicTest';
 export * from './setupContractTest';
 export * from './setupTest';
+export * from './createNode';
--- packages/neo-one-smart-contract-compiler/src/test/setupTest.ts
+++ packages/neo-one-smart-contract-compiler/src/test/setupTest.ts
@@ -15,6 +15,57 @@ import { CompileContractResult } from '../compileContract';
 
 import { createNode } from './createNode';
 
+export function throwErrorOnDiagnosticErrorOrWarning(
+  diagnostics: ReadonlyArray<ts.Diagnostic>,
+  ignoreWarnings?: boolean,
+) {
+  const error = diagnostics.find((diagnostic) => diagnostic.category === DiagnosticCategory.Error);
+  if (error !== undefined) {
+    throw new Error(`Compilation error: ${error.messageText} at ${error.source}`);
+  }
+  const warning = diagnostics.find((diagnostic) => diagnostic.category === DiagnosticCategory.Warning);
+  if (warning !== undefined && !ignoreWarnings) {
+    throw new Error(`Compilation warning: ${warning.messageText} at ${warning.source}`);
+  }
+}
+
+export async function testNodeSetup() {
+  const { privateKey, rpcURL } = await createNode();
+  const networkName = 'priv';
+  const masterWalletName = 'master';
+
+  const keystore = new LocalKeyStore({
+    store: new LocalMemoryStore(),
+  });
+
+  const [masterWallet] = await Promise.all([
+    keystore.addAccount({
+      network: networkName,
+      name: masterWalletName,
+      privateKey,
+    }),
+    // Give RPC server a chance to startup.
+    new Promise<void>((resolve) => setTimeout(resolve, 5000)),
+  ]);
+
+  const provider = new NEOONEProvider({
+    options: [{ network: networkName, rpcURL }],
+  });
+
+  const localUserAccountProvider = new LocalUserAccountProvider({
+    keystore,
+    provider,
+  });
+  const userAccountProviders = {
+    memory: localUserAccountProvider,
+  };
+  const client = new Client(userAccountProviders);
+
+  // Give RPC server a chance to startup.
+
+  return { client, masterWallet, networkName, provider, keystore, privateKey, userAccountProviders };
+}
+
 export interface TestOptions extends CompileContractResult {
   readonly abi: ABI;
   readonly diagnostics: ReadonlyArray<ts.Diagnostic>;
@@ -35,48 +86,21 @@ export interface Result {
 }
 
 export const setupTest = async (getContract: () => Promise<TestOptions>): Promise<Result> => {
-  const { privateKey, rpcURL } = await createNode();
-  const networkName = 'priv';
-  const masterWalletName = 'master';
-
-  const keystore = new LocalKeyStore({
-    store: new LocalMemoryStore(),
-  });
-
-  const masterWallet = await keystore.addAccount({
-    network: networkName,
-    name: masterWalletName,
-    privateKey,
-  });
-
-  const provider = new NEOONEProvider({
-    options: [{ network: networkName, rpcURL }],
-  });
-
-  const localUserAccountProvider = new LocalUserAccountProvider({
-    keystore,
+  const {
+    client,
+    masterWallet,
     provider,
-  });
-  const userAccountProviders = {
-    memory: localUserAccountProvider,
-  };
-  const client = new Client(userAccountProviders);
+    networkName,
+    keystore,
+    privateKey,
+    userAccountProviders,
+  } = await testNodeSetup();
 
   const developerClient = new DeveloperClient(provider.read(networkName), userAccountProviders);
 
   const { contract, sourceMap, diagnostics, abi, ignoreWarnings } = await getContract();
-  const error = diagnostics.find((diagnostic) => diagnostic.category === DiagnosticCategory.Error);
-  if (error !== undefined) {
-    throw new Error(`Compilation error: ${error.messageText} at ${error.source}`);
-  }
 
-  const warning = diagnostics.find((diagnostic) => diagnostic.category === DiagnosticCategory.Warning);
-  if (warning !== undefined && !ignoreWarnings) {
-    throw new Error(`Compilation warning: ${warning.messageText} at ${warning.source}`);
-  }
-
-  // Give RPC server a chance to startup.
-  await new Promise<void>((resolve) => setTimeout(resolve, 5000));
+  throwErrorOnDiagnosticErrorOrWarning(diagnostics, ignoreWarnings);
 
   const result = await client.publish(contract);
 
