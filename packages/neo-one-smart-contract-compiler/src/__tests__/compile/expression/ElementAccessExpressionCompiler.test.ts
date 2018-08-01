a8d2245d4065750e55ef3ed24f7c6cf7e6b80160
Alex DiCarlo <alexdicarlo@gmail.com>
2018-08-01 12:04:25 -0700

    Add ElementAccess coverage

--- packages/neo-one-smart-contract-compiler/src/__tests__/compile/expression/ElementAccessExpressionCompiler.test.ts
+++ packages/neo-one-smart-contract-compiler/src/__tests__/compile/expression/ElementAccessExpressionCompiler.test.ts
@@ -26,6 +26,73 @@ describe('ElementAccessExpressionCompiler', () => {
       if (y[2] !== 2) {
         throw 'Failure';
       }
+
+      if ((y[2] += 1) !== 3) {
+        throw 'Failure';
+      }
+
+      if (y[2] !== 3) {
+        throw 'Failure';
+      }
+    `);
+  });
+
+  test('{ a: 0, b: 1 }[element]', async () => {
+    await helpers.executeString(`
+      const x = { a: 0, b: 1 };
+      if (x['a'] !== 0) {
+        throw 'Failure';
+      }
+
+      if (x['b'] !== 1) {
+        throw 'Failure';
+      }
+
+      if ((x['a'] += 1) !== 1) {
+        throw 'Failure';
+      }
+
+      if (x['a'] !== 1) {
+        throw 'Failure';
+      }
+    `);
+  });
+
+  test('{ [Symbol.for("a")]: 0 }[element]', async () => {
+    await helpers.executeString(`
+      const a = Symbol.for('a');
+      const x = { [a]: 0 };
+      if (x[a] !== 0) {
+        throw 'Failure';
+      }
+
+      if ((x[a] += 1) !== 1) {
+        throw 'Failure';
+      }
+
+      if (x[a] !== 1) {
+        throw 'Failure';
+      }
+    `);
+  });
+
+  test('mixed index types', async () => {
+    await helpers.executeString(`
+      const a: string | number = 0 as string | number;
+      const x = { [a]: 0 };
+      if (x[a] !== 0) {
+        throw 'Failure';
+      }
+
+      if ((x[a] += 1) !== 1) {
+        throw 'Failure';
+      }
+
+      if (x[a] !== 1) {
+        throw 'Failure';
+      }
+
+      x[a];
     `);
   });
 });
