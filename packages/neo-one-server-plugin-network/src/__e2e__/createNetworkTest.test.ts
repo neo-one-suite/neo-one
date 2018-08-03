d8455c3bbe6ec95bb60775110a68409197d32019
Alex DiCarlo <alexdicarlo@gmail.com>
2018-08-02 21:02:49 -0700

    Synchronize to 50 blocks in main and test to make coverage more consistent

--- packages/neo-one-server-plugin-network/src/__e2e__/createNetworkTest.test.ts
+++ packages/neo-one-server-plugin-network/src/__e2e__/createNetworkTest.test.ts
@@ -13,8 +13,8 @@ describe('create network', () => {
       expect(description.nodes[0].name).toEqual('test');
       expect(description.nodes[0].live).toBeTruthy();
       expect(description.nodes[0].ready).toBeFalsy();
-      expect(description.height).toBeGreaterThan(1);
-      expect(description.peers).toBeGreaterThan(1);
+      expect(description.height).toBeGreaterThan(50);
+      expect(description.peers).toBeGreaterThan(50);
     });
   });
 });
