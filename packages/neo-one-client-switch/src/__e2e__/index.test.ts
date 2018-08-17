59e8ef527313ce0ceabe16e69d44d6d43fe343da
Alex DiCarlo <alexdicarlo@gmail.com>
2018-08-17 10:21:40 -0700

    Bump client-switch time to require

--- packages/neo-one-client-switch/src/__e2e__/index.test.ts
+++ packages/neo-one-client-switch/src/__e2e__/index.test.ts
@@ -1,6 +1,6 @@
 describe('@neo-one/client-switch', () => {
   test('time to require', async () => {
     const time = await one.measureRequire('@neo-one/client-switch');
-    expect(time).toBeLessThan(500);
+    expect(time).toBeLessThan(750);
   });
 });
