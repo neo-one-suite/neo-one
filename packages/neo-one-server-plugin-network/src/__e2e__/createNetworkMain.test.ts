463a6ed36e9329ef2aa0e30030b96c4f872a4286
Alex Fragapane <34611820+afragapane@users.noreply.github.com>
2018-08-17 13:39:00 -0600

    Add tests to confirm metrics are received at the expected endpoint (#383)

--- packages/neo-one-server-plugin-network/src/__e2e__/createNetworkMain.test.ts
+++ packages/neo-one-server-plugin-network/src/__e2e__/createNetworkMain.test.ts
@@ -1,4 +1,5 @@
 import { Network } from '../NetworkResourceType';
+import fetch from 'cross-fetch';
 
 describe('create network', () => {
   test('create network main', async () => {
@@ -15,6 +16,14 @@ describe('create network', () => {
       expect(description.nodes[0].ready).toBeFalsy();
       expect(description.height).toBeGreaterThan(5);
       expect(description.peers).toBeGreaterThan(5);
+
+      const endpoint = description.nodes[0].telemetryAddress;
+      const response = await fetch(endpoint);
+      expect(response.ok).toEqual(true);
+
+      const body = await response.text();
+      const metrics = body.split('\n').filter((line) => line.includes('# TYPE') && line.includes('neo_'));
+      expect(metrics).toMatchSnapshot();
     });
   });
 });
