694e99caedc3e0d01f606f7afc0e623da16c6fd1
Alex DiCarlo <alexdicarlo@gmail.com>
2018-11-01 20:21:56 -0700

    Lesson 4 Chapter 6

---
+++ packages/neo-one-website/courses/0-tokenomics/3-escrow/5-generalize/one/tests/Escrow.test.ts
@@ -0,0 +1,221 @@
+// tslint:disable
+import { Hash256 } from '@neo-one/client';
+import BigNumber from 'bignumber.js';
+// @ts-ignore
+import { withContracts } from '../generated/test';
+
+jest.setTimeout(30000);
+
+describe('Escrow', () => {
+  test('holds funds claimable by two parties', async () => {
+    // @ts-ignore
+    await withContracts(async ({ token, escrow, developerClient, masterAccountID, networkName, accountIDs }) => {
+      expect(token).toBeDefined();
+      expect(escrow).toBeDefined();
+
+      // `accountIDs` contains accounts with NEO and GAS and they are preconfigured in the `client`
+      const toAccountID = accountIDs[0];
+
+      // Fast forward to the start of the ICO so that we can mint some tokens.
+      await developerClient.fastForwardOffset(60 * 60);
+      // Mint tokens so we have some available to use in the Escrow
+      const mintNEOAmount = new BigNumber(10);
+      const mintReceipt = await token.mintTokens.confirmed({
+        sendTo: [
+          {
+            amount: mintNEOAmount,
+            asset: Hash256.NEO,
+          },
+        ],
+      });
+      if (mintReceipt.result.state === 'FAULT') {
+        throw new Error(mintReceipt.result.message);
+      }
+      expect(mintReceipt.result.value).toEqual(true);
+
+      // Pre-approve the transfer by the Escrow account
+      const escrowAmount = new BigNumber(100);
+      const additionalAmount = new BigNumber(50);
+      const escrowAddress = escrow.definition.networks[networkName].address;
+      const approveReceipt = await token.approveSendTransfer.confirmed(
+        masterAccountID.address,
+        escrowAddress,
+        escrowAmount.plus(additionalAmount),
+      );
+      if (approveReceipt.result.state === 'FAULT') {
+        throw new Error(approveReceipt.result.message);
+      }
+      expect(approveReceipt.result.value).toEqual(true);
+
+      // Depost into the Escrow account
+      const tokenAddress = token.definition.networks[networkName].address;
+      const escrowReceipt = await escrow.deposit.confirmed(
+        masterAccountID.address,
+        toAccountID.address,
+        escrowAmount,
+        tokenAddress,
+      );
+      if (escrowReceipt.result.state === 'FAULT') {
+        throw new Error(escrowReceipt.result.message);
+      }
+      expect(escrowReceipt.result.value).toEqual(true);
+      let event = escrowReceipt.events[0];
+      expect(event.name).toEqual('transfer');
+      if (event.name !== 'transfer') {
+        throw new Error('For TS');
+      }
+      expect(event.parameters.from).toEqual(masterAccountID.address);
+      expect(event.parameters.to).toEqual(escrowAddress);
+      expect(event.parameters.amount.toNumber()).toEqual(escrowAmount.toNumber());
+      event = escrowReceipt.events[1];
+      expect(event.name).toEqual('balanceAvailable');
+      if (event.name !== 'balanceAvailable') {
+        throw new Error('For TS');
+      }
+      expect(event.parameters.from).toEqual(masterAccountID.address);
+      expect(event.parameters.to).toEqual(toAccountID.address);
+      expect(event.parameters.amount.toNumber()).toEqual(escrowAmount.toNumber());
+      expect(event.parameters.asset).toEqual(tokenAddress);
+
+      // Verify the escrow balance matches the above.
+      let balance = await escrow.balanceOf(masterAccountID.address, toAccountID.address, tokenAddress);
+      expect(balance.toNumber()).toEqual(escrowAmount.toNumber());
+
+      // Try depositing more than is pre-approved
+      const failedDepositReceipt = await escrow.deposit.confirmed(
+        masterAccountID.address,
+        toAccountID.address,
+        additionalAmount.plus(1),
+        tokenAddress,
+      );
+      if (failedDepositReceipt.result.state === 'FAULT') {
+        throw new Error(failedDepositReceipt.result.message);
+      }
+      expect(failedDepositReceipt.result.value).toEqual(false);
+
+      // Verify the balance is still the same since the deposit failed.
+      balance = await escrow.balanceOf(masterAccountID.address, toAccountID.address, tokenAddress);
+      expect(balance.toNumber()).toEqual(escrowAmount.toNumber());
+
+      // Claim half of the escrow balance
+      const claimAmount = escrowAmount.dividedBy(2);
+      const claimReceipt = await escrow.claim.confirmed(
+        masterAccountID.address,
+        toAccountID.address,
+        claimAmount,
+        tokenAddress,
+        // Set the from address as the toAccountID since only the `to` address should be able to claim from the Escrow account.
+        { from: toAccountID },
+      );
+      if (claimReceipt.result.state === 'FAULT') {
+        throw new Error(claimReceipt.result.message);
+      }
+      expect(claimReceipt.result.value).toEqual(true);
+      // Notice how the receipt has the events for both the Token contract we invoked as well as the Escrow contract.
+      event = claimReceipt.events[0];
+      expect(event.name).toEqual('transfer');
+      if (event.name !== 'transfer') {
+        throw new Error('For TS');
+      }
+      expect(event.parameters.from).toEqual(escrowAddress);
+      expect(event.parameters.to).toEqual(toAccountID.address);
+      expect(event.parameters.amount.toNumber()).toEqual(claimAmount.toNumber());
+      event = claimReceipt.events[1];
+      expect(event.name).toEqual('balanceClaimed');
+      if (event.name !== 'balanceClaimed') {
+        throw new Error('For TS');
+      }
+      expect(event.parameters.from).toEqual(masterAccountID.address);
+      expect(event.parameters.to).toEqual(toAccountID.address);
+      expect(event.parameters.amount.toNumber()).toEqual(claimAmount.toNumber());
+      expect(event.parameters.asset).toEqual(tokenAddress);
+
+      // Verify the escrow balance has been deducted
+      balance = await escrow.balanceOf(masterAccountID.address, toAccountID.address, tokenAddress);
+      expect(balance.toNumber()).toEqual(escrowAmount.minus(claimAmount).toNumber());
+
+      // Try claiming the remainder + 1 (i.e. more than the balance of the escrow account)
+      const failedClaimReceipt = await escrow.claim.confirmed(
+        masterAccountID.address,
+        toAccountID.address,
+        claimAmount.plus(1),
+        tokenAddress,
+        { from: toAccountID },
+      );
+      if (failedClaimReceipt.result.state === 'FAULT') {
+        throw new Error(failedClaimReceipt.result.message);
+      }
+      expect(failedClaimReceipt.result.value).toEqual(false);
+
+      // Verify the escrow balance is still the same.
+      balance = await escrow.balanceOf(masterAccountID.address, toAccountID.address, tokenAddress);
+      expect(balance.toNumber()).toEqual(escrowAmount.minus(claimAmount).toNumber());
+
+      // Refund the remainder of the escrow account
+      const refundAmount = escrowAmount.minus(claimAmount);
+      const refundReceipt = await escrow.refund.confirmed(
+        masterAccountID.address,
+        toAccountID.address,
+        refundAmount,
+        tokenAddress,
+      );
+      if (refundReceipt.result.state === 'FAULT') {
+        throw new Error(refundReceipt.result.message);
+      }
+      expect(refundReceipt.result.value).toEqual(true);
+      event = refundReceipt.events[0];
+      expect(event.name).toEqual('transfer');
+      if (event.name !== 'transfer') {
+        throw new Error('For TS');
+      }
+      expect(event.parameters.from).toEqual(escrowAddress);
+      expect(event.parameters.to).toEqual(masterAccountID.address);
+      expect(event.parameters.amount.toNumber()).toEqual(refundAmount.toNumber());
+      event = refundReceipt.events[1];
+      expect(event.name).toEqual('balanceRefunded');
+      if (event.name !== 'balanceRefunded') {
+        throw new Error('For TS');
+      }
+      expect(event.parameters.from).toEqual(masterAccountID.address);
+      expect(event.parameters.to).toEqual(toAccountID.address);
+      expect(event.parameters.amount.toNumber()).toEqual(refundAmount.toNumber());
+      expect(event.parameters.asset).toEqual(tokenAddress);
+
+      // Verify the escrow balance is now 0
+      balance = await escrow.balanceOf(masterAccountID.address, toAccountID.address, tokenAddress);
+      expect(balance.toNumber()).toEqual(0);
+
+      // Try to refund more (and fail)
+      const failedRefundReceipt = await escrow.refund.confirmed(
+        masterAccountID.address,
+        toAccountID.address,
+        new BigNumber(1),
+        tokenAddress,
+      );
+      if (failedRefundReceipt.result.state === 'FAULT') {
+        throw new Error(failedRefundReceipt.result.message);
+      }
+      expect(failedRefundReceipt.result.value).toEqual(false);
+
+      // Verify that claim throws an error in the exceptional case that we pass a negative number
+      let error: Error | undefined;
+      try {
+        await escrow.claim.confirmed(masterAccountID.address, toAccountID.address, new BigNumber(-1), tokenAddress, {
+          from: toAccountID,
+        });
+      } catch (err) {
+        error = err;
+      }
+      expect(error).toBeDefined();
+
+      // Verify that refund throws an error in the exceptional case that we pass a negative number
+      error = undefined;
+      try {
+        await escrow.refund.confirmed(masterAccountID.address, toAccountID.address, new BigNumber(-1), tokenAddress);
+      } catch (err) {
+        error = err;
+      }
+      expect(error).toBeDefined();
+    });
+  });
+});
