379ee295668833096b2bc3e4e324beb0de0f1364
Alex DiCarlo <alexdicarlo@gmail.com>
2018-11-08 11:58:03 -0800

    Fix developer tools frame dependency in build

--- packages/neo-one-developer-tools/src/DeveloperTools.ts
+++ packages/neo-one-developer-tools/src/DeveloperTools.ts
@@ -5,5 +5,6 @@ import { DeveloperToolsOptions } from './types';
 export interface DeveloperTools {
   readonly enable: (options: DeveloperToolsOptions) => void;
 }
+
 export const DeveloperTools =
   process.env.NODE_ENV === 'production' && process.env.NEO_ONE_DEV !== 'true' ? DeveloperToolsProd : DeveloperToolsDev;
