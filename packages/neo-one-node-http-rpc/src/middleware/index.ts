057b084e48b4776525ee71189b597f6dea7ecfe1
Daniel Byrne <danwbyrne@gmail.com>
2018-11-13 13:19:30 -0800

    feat(node): add `tooBusy` and `rateLimit` middleware

--- packages/neo-one-node-http-rpc/src/middleware/index.ts
+++ packages/neo-one-node-http-rpc/src/middleware/index.ts
@@ -1,5 +1,7 @@
 export { liveHealthCheck } from './liveHealthCheck';
 export { readyHealthCheck } from './readyHealthCheck';
+export { tooBusyCheck } from './tooBusyCheck';
 export { rpc } from './rpc';
 export { Options as LiveHealthCheckOptions } from './liveHealthCheck';
 export { Options as ReadyHealthCheckOptions } from './readyHealthCheck';
+export { Options as TooBusyCheckOptions } from './tooBusyCheck';
