1878525a92ae18924912c63c28b48340b303672a
Alex DiCarlo <alexdicarlo@gmail.com>
2018-11-16 09:20:59 -0800

    fix(@neo-one/website): Fix blog/docs redirect in prod

--- packages/neo-one-website/src/pages/blogRedirect.tsx
+++ packages/neo-one-website/src/pages/blogRedirect.tsx
@@ -2,12 +2,11 @@
 // tslint:disable-next-line no-import-side-effect
 import '../polyfill';
 
-import { Loading } from '@neo-one/react-common';
 import { Redirect } from '@reach/router';
 import * as React from 'react';
 import { RouteData } from 'react-static';
 import { Helmet } from '../components';
-import { MainLayout } from '../layout';
+import { BlogLoading } from '../layout';
 
 interface BlogRedirectProps {
   readonly redirect: string;
@@ -15,12 +14,12 @@ interface BlogRedirectProps {
 
 // tslint:disable-next-line:no-default-export export-name
 export default () => (
-  <MainLayout path="blog">
+  <>
     <Helmet title="NEO•ONE Blog" />
     {/*
     // @ts-ignore */}
-    <RouteData Loader={Loading}>
+    <RouteData Loader={BlogLoading}>
       {({ redirect }: BlogRedirectProps) => <Redirect to={redirect} noThrow={typeof window === 'undefined'} />}
     </RouteData>
-  </MainLayout>
+  </>
 );
