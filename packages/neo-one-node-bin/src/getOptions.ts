49d68380a9a92988f7bfa3d701f29d9ff97c0d2f
Alex DiCarlo <alexdicarlo@gmail.com>
2019-08-19 04:32:10 -0700

    Add shortcut for specifying main/test blockchain

--- packages/neo-one-node-bin/src/getOptions.ts
+++ packages/neo-one-node-bin/src/getOptions.ts
@@ -1,5 +1,5 @@
 import { FullNodeCreateOptions } from '@neo-one/node';
-import { createMain } from '@neo-one/node-neo-settings';
+import { createMain, createTest } from '@neo-one/node-neo-settings';
 import envPaths from 'env-paths';
 import rc from 'rc';
 
@@ -17,5 +17,16 @@ const DEFAULT_OPTIONS = {
   blockchain: createMain(),
 };
 
-export const getOptions = (): FullNodeCreateOptions['options'] =>
-  rc('neo-one', DEFAULT_OPTIONS) as FullNodeCreateOptions['options'];
+export const getOptions = (): FullNodeCreateOptions['options'] => {
+  let options = rc('neo-one', DEFAULT_OPTIONS) as FullNodeCreateOptions['options'];
+
+  const { blockchain } = options;
+  if ((typeof blockchain as any) === 'string') {
+    options = {
+      ...options,
+      blockchain: (blockchain as any) === 'test' ? createTest() : createMain(),
+    };
+  }
+
+  return options;
+};
