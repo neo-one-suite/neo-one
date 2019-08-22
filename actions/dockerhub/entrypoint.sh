6fb9931477c8b603a14428d2040ae84203b4d15f
Alex DiCarlo <alexdicarlo@gmail.com>
2019-08-22 10:02:33 -0700

    Fixup #1663

--- actions/dockerhub/entrypoint.sh
+++ actions/dockerhub/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -u -e -E -o pipefail
+
 ## mapping of var from user input or default value
 
 USERNAME=${GITHUB_REPOSITORY%%/*}
@@ -17,18 +19,18 @@ REGISTRY_IMAGE="$NAMESPACE/$IMAGE_NAME"
 ## login if needed
 if [ -n "${DOCKER_PASSWORD+set}" ]
 then
-  sh -c "docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD"
+  docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 fi
 
 ## build the image locally
-sh -c "docker build -t $IMAGE_NAME ${*:-.}" ## pass in the build command from user input, otherwise build in default mode
+docker build -t "$IMAGE_NAME" "${*:-.}" ## pass in the build command from user input, otherwise build in default mode
 
 # push all the tags to registry
-sh -c "docker tag $IMAGE_NAME $REGISTRY_IMAGE:latest"
-sh -c "docker push $REGISTRY_IMAGE:latest"
+docker tag "$IMAGE_NAME $REGISTRY_IMAGE:latest"
+docker push "$REGISTRY_IMAGE:latest"
 
 if [ "${ref_type}" = "tags" ]
 then
-  sh -c "docker tag $IMAGE_NAME $REGISTRY_IMAGE:$IMAGE_TAG"
-  sh -c "docker push $REGISTRY_IMAGE:$IMAGE_TAG"
+  docker tag "$IMAGE_NAME $REGISTRY_IMAGE:$IMAGE_TAG"
+  docker push "$REGISTRY_IMAGE:$IMAGE_TAG"
 fi
