6c79dd7392183777cb221e5a8c74e8673eaf9ba5
afragapane <ahfragapane@gmail.com>
2019-08-22 11:08:54 -0700

    dockeractions really fix this time

--- actions/dockerhub/entrypoint.sh
+++ actions/dockerhub/entrypoint.sh
@@ -19,7 +19,7 @@ REGISTRY_IMAGE="$NAMESPACE/$IMAGE_NAME"
 ## login if needed
 if [ -n "${DOCKER_PASSWORD+set}" ]
 then
-  $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+  docker login --u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 fi
 
 ## build the image locally
