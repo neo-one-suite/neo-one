54fa881f3d8041808d611b93374a9d4ee191b75d
Daniel Byrne <danwbyrne@gmail.com>
2019-08-23 11:01:06 -0700

    tmp(docker): add echos for docker action temporarily

--- actions/dockerhub/entrypoint.sh
+++ actions/dockerhub/entrypoint.sh
@@ -10,11 +10,14 @@ REPOSITORY=${GITHUB_REPOSITORY#*/}
 ref_tmp=${GITHUB_REF#*/} ## throw away the first part of the ref (GITHUB_REF=refs/heads/master or refs/tags/2019/03/13)
 ref_type=${ref_tmp%%/*} ## extract the second element of the ref (heads or tags)
 ref_value=${ref_tmp#*/} ## extract the third+ elements of the ref (master or 2019/03/13)
-
+echo ref_tmp
+echo ref_type
+echo ref_value
 IMAGE_TAG=${ref_value//\//-} ## replace `/` with `-` in ref for docker tag requirement (master or 2019-03-13)
 NAMESPACE=${DOCKER_NAMESPACE:-$USERNAME} ## use github username as docker namespace unless specified
 IMAGE_NAME=${DOCKER_IMAGE_NAME:-$REPOSITORY} ## use github repository name as docker image name unless specified
 REGISTRY_IMAGE="$NAMESPACE/$IMAGE_NAME"
+echo IMAGE_TAG
 
 ## login if needed
 if [ -n "${DOCKER_PASSWORD+set}" ]
