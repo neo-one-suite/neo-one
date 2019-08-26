3e5feb3e565ae77e3d94d9d8397f228c48ab7f61
Alex DiCarlo <alexdicarlo@gmail.com>
2019-08-26 13:34:43 -0700

    Organize imports

--- packages/neo-one-smart-contract-codegen/src/types/genMigrationSmartContract.ts
+++ packages/neo-one-smart-contract-codegen/src/types/genMigrationSmartContract.ts
@@ -1,6 +1,6 @@
 import { ABI } from '@neo-one/client-common';
-import { getMigrationSmartContractName } from './getMigrationSmartContractName';
 import { genSmartContractBase } from './genSmartContractBase';
+import { getMigrationSmartContractName } from './getMigrationSmartContractName';
 
 export const genMigrationSmartContract = (name: string, abi: ABI): string =>
   genSmartContractBase(name, getMigrationSmartContractName(name), abi, true);
