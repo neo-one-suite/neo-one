2d953a92f7693110fe2ce5803e4a0edb79ff5574
Alex DiCarlo <alexdicarlo@gmail.com>
2019-08-27 16:12:29 -0700

    Change format of node endpoint

--- packages/neo-one-node-core/src/net/types.ts
+++ packages/neo-one-node-core/src/net/types.ts
@@ -7,12 +7,11 @@ export interface EndpointConfig {
 // type:host:port
 export type Endpoint = string;
 
-const SEP = '$$';
-export const createEndpoint = ({ type, host, port }: EndpointConfig): Endpoint => [type, host, port].join(SEP);
+export const createEndpoint = ({ type, host, port }: EndpointConfig): Endpoint => `${type}://${host}:${port}`;
 
 export const getEndpointConfig = (endpoint: Endpoint): EndpointConfig => {
-  const [type, host, port] = endpoint.split(SEP);
+  const [type, host, port] = endpoint.split(':');
 
   // tslint:disable-next-line no-any
-  return { type: type as any, host, port: parseInt(port, 10) };
+  return { type: type as any, host: host.slice(2), port: parseInt(port, 10) };
 };
