440b160b27064ed8b6c9e759e0d696bef309e921
Daniel Byrne <danwbyrne@gmail.com>
2019-11-12 14:24:18 -0800

    feat(rush): switch to pnpm

---
+++ packages/neo-one-build-tools/bin/neo-one-tsc.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+const path = require('path');
+const execa = require('execa');
+
+execa(path.resolve(__dirname, '..', 'node_modules', '.bin', 'tsc'), [
+  '--project',
+  '.',
+  '--noEmit',
+]);
