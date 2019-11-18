2058dc79b568d66479507cca4040a216d39fe74e
Daniel Byrne <danwbyrne@gmail.com>
2019-11-18 12:27:23 -0800

    fix(rush): revert back to yarn for website (#1868)

--- packages/neo-one-build-tools-web/src/webpack/common.ts
+++ packages/neo-one-build-tools-web/src/webpack/common.ts
@@ -8,6 +8,7 @@ import { optimization } from './optimization';
 import { rules } from './rules';
 
 const TOOLS_NODE_MODULES = path.resolve(__dirname, '..', '..', 'node_modules');
+const RUSH_NODE_MODULES = path.resolve(__dirname, '..', '..', '..', '..', 'common', 'temp', 'node_modules');
 
 export const common = ({ stage, bundle }: { readonly stage: Stage; readonly bundle: Bundle }): webpack.Configuration =>
   addDefaultRules({
@@ -17,6 +18,7 @@ export const common = ({ stage, bundle }: { readonly stage: Stage; readonly bund
       aliasFields: ['browser'],
       extensions: ['.mjs', '.js', '.jsx', '.json', '.ts', '.tsx'],
       alias,
+      modules: [RUSH_NODE_MODULES, 'node_modules'],
     },
     resolveLoader: {
       modules: [TOOLS_NODE_MODULES, 'node_modules'],
