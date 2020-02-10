8ea0404b3684e6dad96456ae929d2fb82dfaff78
Daniel Byrne <danwbyrne@gmail.com>
2020-02-10 12:50:16 -0800

    fix(test): local unit tests now use ts-jest instead of built packages (#1972)

--- packages/neo-one-build-tests/jest/e2e.js
+++ packages/neo-one-build-tests/jest/e2e.js
@@ -6,11 +6,7 @@ module.exports = {
   testRunner:
     '<rootDir>/packages/neo-one-build-tests/node_modules/jest-circus/runner',
   testRegex: '^.*/__e2e__/.*\\.test\\.tsx?$',
-  moduleNameMapper: {
-    '^@neo-one/ec-key': '@neo-one/ec-key',
-    '^@neo-one/boa': '@neo-one/boa',
-    '^@neo-one/csharp': '@neo-one/csharp',
-  },
+  moduleNameMapper: undefined,
   modulePathIgnorePatterns: [
     '<rootDir>/packages/[a-z-]+/package.json',
     '<rootDir>/common',
