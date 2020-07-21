d5dc995bfdd20034ee861175978f54b53f6bb437
Spencer Corwin <spencercorwin@icloud.com>
2020-07-21 10:31:22 -0700

    fix(tests): update NT dep and website build process (#2106)

--- packages/neo-one-cli/src/__e2e__/cmd/stop/neotracker.test.ts
+++ packages/neo-one-cli/src/__e2e__/cmd/stop/neotracker.test.ts
@@ -17,8 +17,6 @@ describe('start network', () => {
     });
 
     execAsync('start neotracker');
-    // this allows us to run the e2e tests in parallel, successfully
-    await new Promise((resolve) => setTimeout(resolve, 5000));
 
     await one.until(async () => {
       const live = await isRunning(config.neotracker.port);
