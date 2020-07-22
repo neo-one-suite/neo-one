d3a492d4293bd2bb621e6d2729e344cba663b4e3
Spencer Corwin <spencercorwin@icloud.com>
2020-07-21 18:07:12 -0700

    fix(husky): remove checkout hook (#2111)

--- .huskyrc.js
+++ .huskyrc.js
@@ -9,7 +9,6 @@ const hooks =
         'pre-commit': `rush check && rush lint:staged -p 16 && rush build && GIT_DIR=${basePath} rush change -v`,
         'post-merge': 'rush install',
         'post-rewrite': 'rush install',
-        'post-checkout': 'rush install',
       };
 
 module.exports = {
