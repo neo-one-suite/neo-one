5137c77a5da9a2cc98043a61f50939c21eca3299
Tai Nguyen <tainguyen@Tais-MacBook-Pro.local>
2021-02-11 15:54:34 -0800

    fix: client-switch source map initialization issue with Jest

--- packages/neo-one-client-switch/src/node/processActionsAndMessage.ts
+++ packages/neo-one-client-switch/src/node/processActionsAndMessage.ts
@@ -1,4 +1,12 @@
 import { processActionsAndMessage as processActionsAndMessageBase, ProcessActionsAndMessageOptions } from '../common';
+import { initializeSourceMap } from './initializeSourceMap';
 
-export const processActionsAndMessage = async (options: ProcessActionsAndMessageOptions): Promise<string> =>
-  processActionsAndMessageBase(options);
+export const processActionsAndMessage = async (options: ProcessActionsAndMessageOptions): Promise<string> => {
+  if (process.env.NODE_ENV === 'production' && process.env.NEO_ONE_DEV !== 'true') {
+    return options.message;
+  }
+
+  initializeSourceMap();
+
+  return processActionsAndMessageBase(options);
+};
