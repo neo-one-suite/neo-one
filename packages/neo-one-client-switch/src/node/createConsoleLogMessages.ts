58af39658fb007dd0a06e8cc34988a32215d46c4
Tai Nguyen <tainguyen@Tais-MacBook-Pro.local>
2021-02-11 16:11:59 -0800

    fix: client-switch source map initialization issue with Jest

--- packages/neo-one-client-switch/src/node/createConsoleLogMessages.ts
+++ packages/neo-one-client-switch/src/node/createConsoleLogMessages.ts
@@ -1,8 +1,16 @@
 import { RawAction } from '@neo-one/client-common';
 import { createConsoleLogMessages as createConsoleLogMessagesBase, LogOptions, SourceMaps } from '../common';
+import { initializeSourceMap } from './initializeSourceMap';
 
 export const createConsoleLogMessages = async (
   actions: readonly RawAction[],
   sourceMaps: SourceMaps,
   options: LogOptions,
-): Promise<readonly string[]> => createConsoleLogMessagesBase(actions, sourceMaps, options);
+): Promise<readonly string[]> => {
+  if (process.env.NODE_ENV === 'production' && process.env.NEO_ONE_DEV !== 'true') {
+    return [];
+  }
+  initializeSourceMap();
+
+  return createConsoleLogMessagesBase(actions, sourceMaps, options);
+};
